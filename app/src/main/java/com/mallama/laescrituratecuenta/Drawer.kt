d8da4c492ed1cc51afd73ddbe2dcbcc37ce53664
Judith Vanessa Mallama Pascumal <judith.mallama@udea.edu.co>
2022-09-08 08:11:30 -0500

    Navegacion, audio, Read, Write, cambio de iconos II

--- app/src/main/java/com/mallama/laescrituratecuenta/Drawer.kt
+++ app/src/main/java/com/mallama/laescrituratecuenta/Drawer.kt
@@ -26,10 +26,10 @@ class Drawer : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarDrawer.toolbar)
 
-        binding.appBarDrawer.fab.setOnClickListener { view ->
+      /**  binding.appBarDrawer.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }**/
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_drawer)
